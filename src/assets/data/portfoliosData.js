69fb618ba92eea8441cd6f0bea500bf0cf92c7fb
dove <codegeekdove@gmail.com>
2023-11-24 20:13:31 +0800

    updated json files

--- src/assets/data/portfoliosData.js
+++ src/assets/data/portfoliosData.js
@@ -5,22 +5,22 @@ const portfolioData = [
   {
     id: 1,
     title: 'reelestate',
-    description: 'reelestate quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    description: 'This project was the first Mini Project I had during the Bootcamp I joined recently. Its was about realestate listing website kind of thing where sellers can list properties and stuff. It was designed in Figma(wireframe). This project was primarily for me to practice my skills in Vanilla CSS and ensure that I am capapble of making a website without the use of any CSS Frameworks/Libraries',
     icons: [
       {
-        sourse: { icon: FaGithub, link: 'https://github.com/' },
-        achor: { icon: FaLink, link: 'https://live.com/' },
+        sourse: { icon: FaGithub, link: 'https://github.com/dovecancode/novice-craft/tree/main/reelestate' },
+        achor: { icon: FaLink, link: 'https://reelestate.vercel.app/' },
       },
     ],
   },
   {
     id: 2,
     title: 'gadgetworld',
-    description: 'gadgetworld quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    description: 'This website is part of my second Mini Project during the Bootcamp its an eCommerce website type of thing. My group decided to use Bootstrap Framework to make the developement easy and more UI friendly. Since I was in a group I go through a lot of basic commands of git and GitHub. It was also the perect time to explore Vanilla Javascript',
     icons: [
       {
-        sourse: { icon: FaGithub, link: 'https://github.com/' },
-        achor: { icon: FaLink, link: 'https://live.com/' },
+        sourse: { icon: FaGithub, link: 'https://github.com/dovecancode/Gadget-World' },
+        achor: { icon: FaLink, link: 'https://gadget-world.vercel.app/' },
       },
     ],
   },
@@ -30,19 +30,19 @@ const portfolioData = [
     description: 'parspot quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
     icons: [
       {
-        sourse: { icon: FaGithub, link: 'https://github.com/' },
-        achor: { icon: FaLink, link: 'https://live.com/' },
+        sourse: { icon: FaGithub, link: '#' },
+        achor: { icon: FaLink, link: '#' },
       },
     ],
   },
   {
     id: 4,
     title: 'todo list app',
-    description: 'todo list app quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+    description: 'This to-do list app probably is my favorite one so far. Doing this  I got deep dive more into Vanilla Javascript, local storages, and a little bit of CSS animation triggerd by javascript events',
     icons: [
       {
         sourse: { icon: FaGithub, link: 'https://github.com/' },
-        achor: { icon: FaLink, link: 'https://live.com/' },
+        achor: { icon: FaLink, link: 'https://dove-todo-app.vercel.app/' },
       },
     ],
   },
