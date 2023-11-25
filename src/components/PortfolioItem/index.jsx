f9e25dab58277c1c6c2077d242be9b0ca939713a
dove <codegeekdove@gmail.com>
2023-11-25 18:39:03 +0800

    turn portfolioItem  classes to into modular

--- src/components/PortfolioItem/index.jsx
+++ src/components/PortfolioItem/index.jsx
@@ -1,27 +1,25 @@
 import PropTypes from 'prop-types'
-import './style.css'
-
 import { Link } from 'react-router-dom'
+import styles from './style.module.css'
 
 function PortfolioItem({ portfolios }) {
   const { title, description, icons } = portfolios
 
   return (
-    <div className="card">
+    <div className={styles.card}>
       <h3>{title}</h3>
       <p>{description}</p>
-      <div className="options">
-        {icons.map((icon, idx) => (
-          <div className="options" key={`p-icon-${idx}`}>
-            <Link to={icon.sourse.link} target="_blank" rel="noreferrer" title="Github">
-              <icon.sourse.icon />
-            </Link>
-            <Link to={icon.achor.link} target="_blank" rel="noreferrer" title="Live Site">
-              <icon.achor.icon />
-            </Link>
-          </div>
-        ))}
-      </div>
+
+      {icons.map((icon, idx) => (
+        <div className={styles.options} key={`p-icon-${idx}`}>
+          <Link to={icon.sourse.link} target="_blank" rel="noreferrer" title="Github">
+            <icon.sourse.icon />
+          </Link>
+          <Link to={icon.achor.link} target="_blank" rel="noreferrer" title="Live Site">
+            <icon.achor.icon />
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
