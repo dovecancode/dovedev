98a5c4b3ff882f5d7f71efb9d0a19121da5ba7a7
dove <codegeekdove@gmail.com>
2023-11-25 17:59:12 +0800

    turn  menu button classes to into modular css

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import BurgerMenuIcon from '../BurgerMenuIcon'
 import NavMenus from '../NavMenus'
-import './style.css'
+import styles from './style.module.css'
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
-      <div className={`nav-button ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
-        <div className="nav-button-container">
-          <div className="menu">Menu</div>
+      <div className={`${styles.navButton} ${isOpen ? styles.open : ''}`} onClick={() => setIsOpen(!isOpen)}>
+        <div className={styles.navButtonContainer}>
+          <div className={styles.menu}>Menu</div>
           <BurgerMenuIcon isOpen={isOpen} />
         </div>
       </div>
