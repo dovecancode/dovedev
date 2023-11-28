33a768b4f6f7c49366f76391a88695685102573b
dove <codegeekdove@gmail.com>
2023-11-28 17:04:38 +0800

    updated json files

--- src/assets/data/portfoliosData.js
+++ src/assets/data/portfoliosData.js
@@ -13,17 +13,17 @@ const portfolioData = [
       },
     ],
   },
-  {
-    id: 2,
-    title: 'gadgetworld',
-    description: 'This website is part of my second Mini Project during the Bootcamp its an eCommerce website type of thing. My group decided to use Bootstrap Framework to make the developement easy and more UI friendly. Since I was in a group I go through a lot of basic commands of git and GitHub. It was also the perect time to explore Vanilla Javascript',
-    icons: [
-      {
-        sourse: { icon: FaGithub, link: 'https://github.com/dovecancode/Gadget-World' },
-        achor: { icon: FaLink, link: 'https://gadget-world.vercel.app/' },
-      },
-    ],
-  },
+  // {
+  //   id: 2,
+  //   title: 'gadgetworld',
+  //   description: 'This website is part of my second Mini Project during the Bootcamp its an eCommerce website type of thing. My group decided to use Bootstrap Framework to make the developement easy and more UI friendly. Since I was in a group I go through a lot of basic commands of git and GitHub. It was also the perect time to explore Vanilla Javascript',
+  //   icons: [
+  //     {
+  //       sourse: { icon: FaGithub, link: 'https://github.com/dovecancode/Gadget-World' },
+  //       achor: { icon: FaLink, link: 'https://gadget-world.vercel.app/' },
+  //     },
+  //   ],
+  // },
   {
     id: 3,
     title: 'parkspot',
