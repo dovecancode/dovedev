d732f6c777ca8cb12b792d7f12df198bb7ac7504
dove <codegeekdove@gmail.com>
2023-12-06 10:24:10 +0800

    fixed the hover effect for source icons

--- src/components/SocialMediaList/index.jsx
+++ src/components/SocialMediaList/index.jsx
@@ -2,7 +2,7 @@ import { FaCodepen, FaEnvelope, FaGithub } from 'react-icons/fa'
 import Item from '../SocialMediaItem'
 import styles from './style.module.css'
 
-const socialMedia = [
+const iconSource = [
   { id: 1, icon: <FaGithub />, link: 'https://github.com/dovecancode' },
   { id: 2, icon: <FaCodepen />, link: 'https://codepen.io/dovecancode' },
   { id: 3, icon: <FaEnvelope />, link: 'mailto:ndoverte11@gmail.com' },
@@ -10,9 +10,9 @@ const socialMedia = [
 
 function SocialMediaList() {
   return (
-    <ul className={styles['social-media']}>
-      {socialMedia.map((social) => (
-        <Item key={`list-${social.id}`} item={social} />
+    <ul className={styles['p-link']}>
+      {iconSource.map((icon) => (
+        <Item key={`list-${icon.id}`} item={icon} />
       ))}
     </ul>
   )
